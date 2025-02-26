876a669f393d30bf46a1b5436b20152cdb884216
Ramky <uramakrishna21@gmail.com>
2025-02-26 11:20:31 +0100

    constant (Variables) in training pipeline got updated

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import os
-import networksecurity.constant import training_pipeliNE
+from networksecurity.constant import training_pipeline
 
 print(training_pipeline.ARTIFACT_DIR)
+print(training_pipeline.FILE_NAME)
 
 class TrainingPipelineConfig:
     def __init__(self):
