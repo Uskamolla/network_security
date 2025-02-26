57885ebda4c9dc6f195eb1088db576034f0dda1a
Ramky <uramakrishna21@gmail.com>
2025-02-26 14:10:08 +0100

    data ingestion pipeline copleted and working now

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -34,11 +34,17 @@ from networksecurity.entity.artifact_entity import(
 
 class TrainingPipeline:
     def __init__(self):
-        pass
+        self.training_pipeline_config = TrainingPipelineConfig()
+
 
     def data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config= DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("starting Data Ingestion")
+            data_ingestion  = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
     
@@ -66,7 +72,7 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def model_pusher(self)
+    def model_pusher(self):
         try:
             pass
         except Exception as e:
@@ -74,7 +80,8 @@ class TrainingPipeline:
         
     def run_pipeline(self):
         try:
-            pass
+            data_ingestion_artifact=self.data_ingestion()
+            print(data_ingestion_artifact)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
