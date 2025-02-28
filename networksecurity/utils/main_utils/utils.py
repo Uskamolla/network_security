01136caa7fdd65710e8a972096fc6f4ff88b6b1f
Ramky <uramakrishna21@gmail.com>
2025-02-28 11:32:35 +0100

    Utils related to data_transformation component and DataTransformation component code updated

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -25,3 +25,44 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    try:
+        """
+        Save numpy array data to file
+        file_path: str location of file to save
+        array: np.array data to save
+        """
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+
+def save_object():
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class") 
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)    
+    
