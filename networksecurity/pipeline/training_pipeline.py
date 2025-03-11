94395c28f5cf46f832eba57ed98633779e246cf3
Ramky <uramakrishna21@gmail.com>
2025-03-11 14:17:09 +0100

    updated training_pipeline ,corrected indentation error

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -112,7 +112,7 @@ class TrainingPipeline:
          except Exception as e:
              raise NetworkSecurityException(e,sys)
              
-     def sync_saved_model_dir_to_s3(self):
+    def sync_saved_model_dir_to_s3(self):
          try:
              aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
              self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_bucket_url=aws_bucket_url)
