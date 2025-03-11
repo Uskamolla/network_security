fa08690eee2a6c9c3270b14319fc21c77a49445f
Ramky <uramakrishna21@gmail.com>
2025-03-11 23:03:16 +0100

    updated code without all the errors

--- networksecurity/cloud/s3_syncer.py
+++ networksecurity/cloud/s3_syncer.py
@@ -5,6 +5,6 @@ class S3Sync:
          command = f"aws s3 sync {folder} {aws_bucket_url} "
          os.system(command)
  
-     def sync_folder_from_s3(self,folder,aws_bucket_url):
+    def sync_folder_from_s3(self,folder,aws_bucket_url):
          command = f"aws s3 sync  {aws_bucket_url} {folder} "
          os.system(command)
