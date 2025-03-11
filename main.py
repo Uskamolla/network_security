8afbe66514400ff17d69f188476ecdc8cb448e95
Ramky <uramakrishna21@gmail.com>
2025-03-11 14:04:33 +0100

    updated the main.py file

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import os
 import sys
 
+import certifi
+ca = certifi.where()
+
+from dotenv import load_dotenv
+load_dotenv()
+mongo_db_url = os.getenv("MONGODB_URL_KEY")
+print(mongo_db_url)
+
+
+AWS_ACCESS_KEY_ID=os.getenv("AWS_ACCESS_KEY_ID_ENV_KEY")
+AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY_ENV_KEY")
+
+os.environ["AWS_ACCESS_KEY_ID"] = AWS_ACCESS_KEY_ID
+os.environ["AWS_SECRET_ACCESS_KEY"]=  AWS_SECRET_ACCESS_KEY
+
+import pymongo
+from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME
+from networksecurity.constant.training_pipeline import DATA_INGESTION_DATABASE_NAME
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
+
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
+
+
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile
@@ -11,32 +34,39 @@ from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
 
-def set_env_variable(env_file_path):
-    pass
+client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+
+database = client[DATA_INGESTION_DATABASE_NAME]
+collection = database[DATA_INGESTION_COLLECTION_NAME]
+
 
 app = FastAPI()
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@app.get("/", tags=["authentication"])
-async def index():
-    return RedirectResponse(url="/docs")
+# @app.get("/", tags=["authentication"])
+# async def index():
+#     return RedirectResponse(url="/docs")
 
 @app.get("/train")
 async def train_route():
     try:
-        pass
+        train_pipeline = TrainingPipeline()
+        if train_pipeline.is_pipeline_running:
+            return Response(content="Pipeline is already running", status_code=200)
+        train_pipeline.run_pipeline()
+        
     except Exception as e:
             raise NetworkSecurityException(e,sys)
     
-
+'''
 @app.get("/predict")
 async def predict_route(request:Request,file: UploadFile = File(...)):
     try:
@@ -51,9 +81,16 @@ def main():
         training_pipeline.run_pipeline() 
     except Exception as e:
             raise NetworkSecurityException(e,sys)
+'''
 
+def main():
+    try:
+        train_pipeline = TrainingPipeline()
+        train_pipeline.run_pipeline()
+        
+    except Exception as e:
+            raise NetworkSecurityException(e,sys)
+     
                
 if __name__=="__main__":
-    main()
-    set_env_variable(env_file_path)
-    app_run(app, host=APP_HOST, port=APP_PORT)
+    app_run(app, host="localhost", port=APP_PORT)
